fbf77df32101cabbcf615233c0d042aad784cd03
dhian ary <usm.dhianary@gmail.com>
2025-10-27 21:06:18 +0700

    first init

---
+++ app/src/main/java/com/example/aplikasipertama/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.aplikasipertama
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val username = findViewById<EditText>(R.id.editTextUsername)
+        val password = findViewById<EditText>(R.id.editTextPassword)
+
+        var usernameText = username.text.toString()
+        var passwordText = password.text.toString()
+
+        var buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
+        buttonSubmit.setOnClickListener {
+            if(usernameText.isBlank() || passwordText.isBlank()){
+                Toast.makeText(this,
+                    "Username & Password tidak boleh kosong",
+                Toast.LENGTH_LONG
+                ).show()
+            }else{
+                Toast.makeText(this,
+                    "Login diproses..",
+                Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
